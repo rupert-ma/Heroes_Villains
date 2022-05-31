9bd31bcf37189858fc7310f26ae2de4a5c30cb1b
rupert-ma <michael.a.rupert@gmail.com>
2022-05-31 08:57:44 -0700

    created Power model in supers models.py

--- supers/admin.py
+++ supers/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Super
+from .models import Power, Super
 
 # Register your models here.
 
 admin.site.register(Super)
+admin.site.register(Power)
--- supers/models.py
+++ supers/models.py
@@ -9,3 +9,6 @@ class Super(models.Model):
     secondary_ability = models.CharField(max_length=255)
     catchphrase = models.CharField(max_length=255)
     super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
+
+class Power(models.Model):
+    name = models.CharField(max_length=255)
