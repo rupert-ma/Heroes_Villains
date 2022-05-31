fa74baa922e1b48b527554e6c434e7da50a90651
rupert-ma <michael.a.rupert@gmail.com>
2022-05-31 07:55:14 -0700

    created get all supertypes, create a new supertype, and get supertype by ide functionality in super_types views.py

--- heroes_villains_project/urls.py
+++ heroes_villains_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/supers/', include('supers.urls')),
-    path('api/supers/', include('super_types.urls')),
+    path('api/supertypes/', include('super_types.urls')),
 ]
--- super_types/views.py
+++ super_types/views.py
@@ -4,14 +4,29 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from .models import Super
-from .serializers import SuperSerializer
+from .models import SuperType
+from .serializers import SuperTypeSerializer
 
 
 
 class Super_Type_List(APIView):
-    pass
+    
+    def get(self, request, format = None):
+        super_types = SuperType.objects.all()
+        serializer = SuperTypeSerializer(super_types, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+    def post(self, request, format = None):
+        serializer = SuperTypeSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 
 class Super_Type_Detail(APIView):
-    pass
+    def get_object(self, pk):
+        try:
+            return SuperType.objects.get(pk=pk)
+        except SuperType.DoesNotExist:
+            raise Http404
